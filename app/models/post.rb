de07d910a694b11853eef77ee8b0680bb40c655b
Jasmine Elkins <jasmine.elkins@gmail.com>
2022-04-26 15:41:12 -0400

    All tests passing

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+
+  #   validate :pass_test
+
+  #   def pass_test
+  #     errors.add(:title, 'title not click-baitey enough') if title == 'True Facts'
+  #   end
+
+  validate :clickbait
+
+  def clickbait
+    unless title =~ /Won't Believe|Secret|Top [0-9]+|Guess/
+      errors.add(:title, 'title not click-baitey enough')
+    end
+  end
 end
